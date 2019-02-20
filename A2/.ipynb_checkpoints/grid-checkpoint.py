0f127f845e36b12133ea8181c4016624ce4a7d7e
Jalil Moussa <abdeljalil.moussa@epfl.ch>
2019-02-20 14:03:45 +0100

    compiles

---
+++ A2/.ipynb_checkpoints/grid-checkpoint.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 18 17:51:11 2019
+
+@author: Jalil M
+"""
+
+import random
+
+from robot import Direction
+
+
+class Grid:
+    robot = None
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.robot_location = random.randint(0, width - 1), random.randint(0, height - 1)
+        self.robot_dir = Direction.random()
+
+    def robot_adj_location(self):
+        x, y = self.robot_location
+        p_s = [(x-1,y-1),(x-1,y),(x-1,y+1),(x,y-1),(x,y),(x,y+1),(x+1,y-1),(x+1,y),(x+1,y+1)]
+        adj = p_s[random.randint(0, 7)]
+
+        # wall check
+        if adj[0] >= self.width or adj[1] >= self.height or adj[0] < 0 or adj[1] < 0:
+            return None
+        else:
+            return adj
+
+    def robot_adj2_location(self):
+		
+        x, y = self.robot_location
+        ps_2 = [(x-2, y-2), (x-2, y-1), (x-2, y), (x-2, y+1), (x-2, y+2), (x-1, y-2),(x-1, y+2), (x, y-2), (x, y+2), (x+1, y-2), (x+1, y+2), (x+2, y-2), (x+2, y-1),(x+2, y), (x+2, y+1), (x+2, y+2)]
+        adj = ps_2[random.randint(0, 15)]
+
+        # wall check
+        if adj[0] >= self.width or adj[1] >= self.height or adj[0] < 0 or adj[1] < 0:
+            return None
+        else:
+            return adj
+
+    def robot_faces_wall(self):
+        x, y = self.robot_location
+
+        # NORTH, EAST, SOUTH, WEST
+        next_l = [(x, y + 1), (x + 1, y), (x, y - 1), (x - 1, y)]
+        next_coord = next_l[self.robot_dir]
+        if next_coord[0] >= self.width or next_coord[1] >= self.height or next_coord[0] < 0 or next_coord[1] < 0:
+            return True
+        else:
+            return False
+
+    def move_robot(self):
+
+        # 30% Chance to change direction.
+        rand = random.random()
+        if rand <= 0.3:
+            self.robot_dir = Direction.random(self.robot_dir)
+        # Changes direction until robot doesn't face wall.
+        while self.robot_faces_wall():
+            self.robot_dir = Direction.random(self.robot_dir)
+
+        x, y = self.robot_location
+
+        # Moves forward in robot's direction.
+        # NORTH, EAST, SOUTH, WEST
+        next_locations = [(x, y + 1), (x + 1, y), (x, y - 1), (x - 1, y)]
+        self.robot_location = next_locations[self.robot_dir]
---
+++ A2/.ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 17 11:39:43 2019
+
+@author: Jalil M
+"""
+
+from time import sleep
+import argparse
+
+from grid import Grid
+from hmm import HMM
+from robot import Robot, Sensor
+
+def start_robot(size):
+    """
+    Sets up the world and robot and enters endless loop of guessing.
+    :param size:
+    """
+    grid = Grid(size.width, size.height)
+    sensor = Sensor(grid)
+    hmm = HMM(size.width, size.height)
+    robot = Robot(sensor, hmm)
+    moves = 0
+    guessed_r = 0
+    while True:
+        grid.move_robot()
+        moves += 1
+        print ("\nRobot position: ", grid.robot_location)
+        guessed, probability = robot.guess_move()
+        if guessed == grid.robot_location:
+            guessed_r += 1
+        man_distance = abs(guessed[0] - grid.robot_location[0]) + abs(guessed[1] - grid.robot_location[1])
+        print ("Manhattan distance: ", man_distance)
+        print ("Robot has an accuracy of:", float(guessed_r) / moves, "during this time.")
+        sleep(1)
+
+if __name__ == '__main__':
+    """
+    The main function called when main.py is run from the command line.
+    Example usage:
+    > python main.py --width 10 --height 10
+    First argument defines the width of the grid, second, the height.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--width", type=int, required=True)
+    parser.add_argument("--height", type=int, required=True)
+    args = parser.parse_args()
+    if args.width <= 1 or args.height <= 1:
+        raise argparse.ArgumentTypeError("Values have to be >1")
+    
+    start_robot(args)
+
