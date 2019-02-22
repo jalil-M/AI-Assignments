3e80ca067786bf8683b527ed1f09003e59ef4b80
ErwanBlain <e.big@hotmail.fr>
2019-02-22 18:21:40 +0100

    Update main.py

--- A2/main.py
+++ A2/main.py
@@ -22,7 +22,8 @@ def start_robot(size):
     robot = Robot(sensor, hmm)
     moves = 0
     guessed_r = 0
-    while True:
+    total_man_distance = 0
+    for i in range(100):
         grid.move_robot()
         moves += 1
         print ("\nRobot position: ", grid.robot_location)
@@ -30,10 +31,12 @@ def start_robot(size):
         if guessed == grid.robot_location:
             guessed_r += 1
         man_distance = abs(guessed[0] - grid.robot_location[0]) + abs(guessed[1] - grid.robot_location[1])
+        total_man_distance += man_distance
         print ("Manhattan distance: ", man_distance)
+        print("Average Manhattan distance", total_man_distance / moves)
         print ("Robot has an accuracy of:", float(guessed_r) / moves, "during this time.")
-        sleep(1)
-
+#        sleep(1)
+    
 if __name__ == '__main__':
     """
     USAGE :
